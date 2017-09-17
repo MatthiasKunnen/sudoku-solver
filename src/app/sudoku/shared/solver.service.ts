6497e8767d0e34d36c304523818fab64ca9fa041
Matthias Kunnen <matthias.kunnen@gmail.com>
2017-09-17 19:25:27 +0200

    Implemented conventional solver

--- src/app/sudoku/shared/solver.service.ts
+++ src/app/sudoku/shared/solver.service.ts
@@ -10,6 +10,7 @@ export class SolverService {
 
     private logObservable: Observable<string>;
     private logObserver: Observer<string>;
+    private solved = false;
     private sudoku: Sudoku;
 
     public constructor(sudoku: Sudoku, config?: Partial<SolverService>) {
@@ -28,6 +29,42 @@ export class SolverService {
      * Start solving the sudoku.
      */
     public start() {
-        throw Error('Not implemented');
+        this.sudoku.runCompletePossibilityCheck();
+
+        // Run loop until it fails, then try LPE
+        while (!this.solved && (this.loop() || this.sudoku.runLPE() > 0)) {
+            this.loop();
+        }
+
+        if (this.completionCallback !== undefined) {
+            this.completionCallback(this.sudoku);
+        }
+    }
+
+    /**
+     * Start a solving loop. When false is returned, the sudoku cannot be
+     * further solved using conventional methods.
+     * @returns {boolean} true if a change occurred. False otherwise.
+     */
+    private loop(): boolean {
+        let changed = false;
+        while (this.sudoku.runUniquePossibility() > 0) {
+            changed = true;
+        }
+
+        if (this.sudoku.isSolved()) {
+            this.solved = true;
+            return true;
+        }
+
+        while (this.sudoku.runSinglePossibility() > 0) {
+        }
+
+        if (this.sudoku.isSolved()) {
+            this.solved = true;
+            return true;
+        }
+
+        return changed;
     }
 }

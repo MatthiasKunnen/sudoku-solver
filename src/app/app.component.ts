444991cec889aec496f567b762b433ee0fa70362
Matthias Kunnen <matthias.kunnen@gmail.com>
2017-09-17 19:56:06 +0200

    Added solver interface to GUI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SolverService } from './sudoku/shared/solver.service';
 import { Sudoku } from './sudoku/shared/sudoku.model';
 import { SudokuService } from './sudoku/shared/sudoku.service';
 
@@ -10,12 +11,40 @@ import { SudokuService } from './sudoku/shared/sudoku.service';
 })
 export class AppComponent implements OnInit {
     public sudoku: Sudoku;
+    public results: Array<string>;
 
     constructor(private sudokuService: SudokuService) {
+        this.results = [];
     }
 
     ngOnInit(): void {
         this.sudokuService.getActiveSudoku()
-            .subscribe(sudoku => this.sudoku = sudoku);
+            .subscribe(sudoku => {
+                this.results = [];
+                this.sudoku = sudoku;
+            });
+    }
+
+    solve(): void {
+        const solver = new SolverService(this.sudoku);
+        solver.start();
+    }
+
+    runCompletePossibilityCheck(): void {
+        this.results.push(`Possibilities were ${this.sudoku.runCompletePossibilityCheck()
+            ? ''
+            : 'not'} eliminated`);
+    }
+
+    runLPE(): void {
+        this.results.push(`LPE eliminated ${this.sudoku.runLPE()} possibilities`);
+    }
+
+    runSingleSolver(): void {
+        this.results.push(`Single solver filled in ${this.sudoku.runSinglePossibility()} cell(s)`);
+    }
+
+    runUniqueSolver(): void {
+        this.results.push(`Unique solver filled in ${this.sudoku.runUniquePossibility()} cell(s)`);
     }
 }
